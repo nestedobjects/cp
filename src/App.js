baa3e18df1915ff0bf7ce6647f198154420a1b95
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-08-26 09:36:41 +0530

    page links

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { Header,Divider,Form, TextArea, Button, Modal, Loader, Dimmer, Input } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
-import { BitlyClient } from 'bitly';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
-const bitly = new BitlyClient('<accesskey>', {});
 const QRCode = require('qrcode.react');
 const styles = {
   body: {
@@ -38,6 +36,9 @@ const styles = {
       marginLeft: 5,
       fontSize: 10,
       borderRadius: 5
+    },
+    copyWidth: {
+      width: 230
     }
 }
 class App extends Component {
@@ -46,50 +47,71 @@ class App extends Component {
     this.state = {
       areaContent: '',
       link: '',
-      copied: false
+      copied: false,
+      status: ''
     }
   }
   getTinyUrl() {
-    bitly
-      .shorten(encodeURI(window.location.href))
-      .then((result) => {
-        if(result.url) {
-          this.setState({link: result.url});
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    this.setState({link: '', status: ''});
+    const dynamicLink = {
+      "longDynamicLink": `https://cpjs.page.link/?link=${window.location.href}`,
+      "suffix": {
+        "option": "SHORT"
+      }
+    };
+    fetch('https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyBiOe8HRoH0KQR5tkgP3Qna_JZCVc7bnz0', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dynamicLink)
+    })
+    .then(res => {
+      if(res.status === 200)
+        return res.json();
+      else
+        return res;
+    })
+    .then(res => {
+      if(res.shortLink)
+        this.setState({link: res.shortLink});
+      else
+        this.setState({status: 'Something wrong in the server!'});
+    })
+    .catch(err => {
+      this.setState({status: 'Something wrong in the server!'})
+    })
   } 
   componentWillMount() {
-    this.setState({areaContent: decodeURI(decodeURI(window.location.search.substring(1)))})
+    this.setState({areaContent: decodeURIComponent(decodeURIComponent(window.location.search.substring(1))).replace(/\+/g, ' ')})
   }
   createLink(e) {
-    window.history.replaceState(null, null, `/?${encodeURI(e.target.value)}`);
+    window.history.replaceState(null, null, `/?${encodeURIComponent(e.target.value)}`);
     this.setState({areaContent: e.target.value});
   }
   render() {
     return (
       <div className="App" style={styles.body}>
           <Header as='h2' content='Copy' style={styles.heading} />
-          <Header as='h6' content="Copy paste across the devices with ease" style={styles.heading} />
+          <Header as='h4' content="Copy paste across the devices with ease" style={styles.heading} />
           <Divider />
           <Form style={styles.areaPadding}>
-            <TextArea autoHeight onChange={this.createLink.bind(this)} style={styles.areaHeight} placeholder='Try adding some links/text' value={this.state.areaContent} />
+            <TextArea autoHeight onChange={this.createLink.bind(this)} maxLength="7168" style={styles.areaHeight} placeholder='Try adding some links/text' value={this.state.areaContent} />
           </Form>
           <div style={styles.areaAction}>
           <Modal
             trigger={<Button content='Copy' primary onClick={this.getTinyUrl.bind(this)} />}>
-              <Modal.Header>You're ready to share!!!</Modal.Header>
+              <Modal.Header>{(this.state.status.length ? <span>Something went wrong!</span> : <span>Congrats! Your links are ready!!!</span>)}</Modal.Header>
                   <Modal.Content >
                     <Modal.Description style={styles.areaAction}>
-                        <QRCode value={window.location.href} />
-                        {(this.state.link.length === 0 ? <Dimmer active inverted>
+                    {(this.state.link.length === 0 && this.state.status.length === 0 ?  <Dimmer active inverted>
                           <Loader inverted>Loading</Loader>
-                        </Dimmer> : <div><br/>(or)<br/><br/><CopyToClipboard text={this.state.link}
+                        </Dimmer> :  this.state.status.length !== 0 ? <span>{this.state.status}</span> : this.state.link.length !== 0 ? <div> <QRCode value={this.state.link} /> <br/>(or)<br/><br/><CopyToClipboard text={this.state.link}
                                     onCopy={() => this.setState({copied: true})}>
-                                    <span style={styles.input} ><Input icon='copy outline' iconPosition='right' value={this.state.link} readOnly /></span>
-                                  </CopyToClipboard>{this.state.copied? <span style={styles.copyStatus}>Copied!</span>: null}</div>)}
+                                    <span style={styles.input} ><Input icon='copy outline' style={styles.copyWidth} iconPosition='right' value={this.state.link} readOnly /></span>
+                                  </CopyToClipboard>{this.state.copied? <span style={styles.copyStatus}>Copied!</span>: null}</div> : null)}
+                    {(this.state.status.length ? <span><br/><br/>Read more about unsupported Characters at <a href="http://unicode.org/faq/unsup_char.html" rel="noopener noreferrer" target="_blank">unicode.org/faq/unsup_char.html</a></span> : null)}
                     </Modal.Description>
                   </Modal.Content>
             </Modal>
